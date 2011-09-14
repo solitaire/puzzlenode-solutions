6dcb7159e9c752aaf6c078797bcebb68bacd88fb
solitaire <solitaire@g.pl>
2011-09-14 18:49:54 +0200

    added encryption/decryption funcionality for both upper and lowercase types of words

--- secret_messages/lib/caesar.rb
+++ secret_messages/lib/caesar.rb
@@ -2,17 +2,20 @@ module Cipher
   class Caesar
     class << self
       def encrypt(text, shift)
-              
+        encrypted_text = "" 
+        text.each_char {|c| encrypted_text += shift_letter(c, shift)}   
+        encrypted_text  
       end
       
       def decrypt(text, shift)
-        
+        encrypt(text, -shift)
       end
       
       def shift_letter(letter, shift)
-        shift = shift % 26
         letter_code = letter.upcase
-        letter_code = letter.ord + shift
+        return letter_code unless letter_code.between?('A', 'Z')
+        shift = shift % 26
+        letter_code = letter_code.ord + shift
         letter_code += 26 if letter_code < 65
         letter_code -= 26 if letter_code > 90
         letter_code.chr
--- secret_messages/spec/caesar_spec.rb
+++ secret_messages/spec/caesar_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
+OUTPUT_TEXT = "FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV; WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.
+              Z. VKDNHVSHDUH, MXOLXV FDHVDU"
+
+INPUT_TEXT = "COWARDS DIE MANY TIMES BEFORE THEIR DEATHS; THE VALIANT NEVER TASTE OF DEATH BUT ONCE.
+              W. SHAKESPEARE, JULIUS CAESAR"
+               
 describe Cipher::Caesar do
   
   describe "#shift_letter" do
@@ -19,5 +25,29 @@ describe Cipher::Caesar do
       end
     end
     
+  end
+  
+  describe "#encrypt" do
+    it "should encrypt a word with the given shift key" do
+      Cipher::Caesar.encrypt("ABCDEF",22).should == "WXYZAB"
+    end
+    
+    it "should encrypt a text with the given shift key" do
+      Cipher::Caesar.encrypt(INPUT_TEXT, 3).should == OUTPUT_TEXT
+    end
+    
+    it "should encrypt a lower-case word with the given key" do
+      Cipher::Caesar.encrypt("abcdef",22).should == "WXYZAB"
+    end
+  end
+  
+  describe "#decrypt" do
+    it "should decrypt a word with the given shift key" do
+      Cipher::Caesar.decrypt("WXYZAB",22).should == "ABCDEF"
+    end
+    
+    it "should decrypt a text with the given shift key" do
+      Cipher::Caesar.decrypt(OUTPUT_TEXT, 3).should == INPUT_TEXT
+    end
   end
 end
