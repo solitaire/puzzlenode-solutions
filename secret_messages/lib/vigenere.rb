cf5e0ade472c1031fded7271d0f7f546627166bb
solitaire <solitaire@g.pl>
2011-09-15 11:50:23 +0200

    added vigenere encryption/decryption

--- secret_messages/lib/vigenere.rb
+++ secret_messages/lib/vigenere.rb
@@ -1,5 +1,35 @@
+require 'utils'
 module Cipher
   class Vigenere
-    
+    class << self
+      include Utils
+      def encrypt(text, key) 
+        shift_text(text, key) {|code, shift| shift_letter(code, shift)}
+      end
+      
+      def decrypt(text, key)
+        shift_text(text, key) {|code, shift| shift_letter(code, -shift)}
+      end
+      
+      private
+      def shift_text(text, key)
+        result = ""
+        key_length = key.length
+        index = 0
+        text.each_char do |c|
+          if c =~ /[a-zA-Z]/   
+            result += yield(c, get_shift_value(key[index]))
+            index = (index+1) % key_length
+          else
+            result += c
+          end
+        end
+        result
+      end
+      def get_shift_value(letter)
+        if letter.ord.between?(UPPER_START, UPPER_END) then return letter.ord - UPPER_START
+        elsif letter.ord.between?(LOWER_START, LOWER_END) then return letter.ord - LOWER_START end
+      end
+    end
   end
 end
--- secret_messages/spec/vigenere_spec.rb
+++ secret_messages/spec/vigenere_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
+INPUT_TEXT = "COWARDS DIE MANY TIMES BEFORE THEIR DEATHS; THE VALIANT NEVER TASTE OF DEATH 
+BUT ONCE. OF ALL THE WONDERS THAT I YET HAVE HEARD, IT SEEMS TO ME MOST STRANGE 
+THAT MEN SHOULD FEAR; SEEING THAT DEATH, A NECESSARY END, WILL COME WHEN 
+IT WILL COME."
+
+OUTPUT_TEXT = "IONDVQY DZH QNTY KLQRY BVISEK TYHME JERWLF; ZHV YEYOAEW RRBEI WEFZE FI HRGTY 
+EYG UNTH. SS GLC WLR COEGIEY TYDX V EEK KEIK HVDVQ, OT JHIZY TF PI ZUSK VXEGNXH 
+XUGT DHR FNOLOH SKAI; VIRONX WLNZ DVDXU, G NVFIFYAIB IAJ, WZOP PUMV ZLRT 
+IK ZMYR CFPI."
+
+KEY = "GARDEN"
+
 describe Cipher::Vigenere do
+  describe "#encrypt" do
+    it "should encrypt a text with the specified key" do
+      Cipher::Vigenere.encrypt(INPUT_TEXT, KEY).should == OUTPUT_TEXT
+    end
+  end
   
+  describe "#decrypt" do
+    it "should descrypt a text with the specified key" do
+      Cipher::Vigenere.decrypt(OUTPUT_TEXT, KEY).should == INPUT_TEXT
+    end
+  end
 end
